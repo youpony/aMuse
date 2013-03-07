8b47742e128c21b85cc2489ac7fbca44d44b547e
Mattia Larentis <mattia@larentis.eu>
2013-03-07 18:57:33 +0100

    fix static files and include some javascript libraries

--- muse/settings.py
+++ muse/settings.py
@@ -1,4 +1,5 @@
 # Django settings for muse project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -26,6 +27,7 @@ DATABASES = {
     }
 }
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -63,7 +65,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -117,6 +119,8 @@ TEMPLATE_DIRS = (
 
 INSTALLED_APPS = (
     'muse.kiosk',
+    'muse.rest',
+
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -127,7 +131,6 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'muse.rest',
 )
 
 # A sample logging configuration. The only tangible logging
