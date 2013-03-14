826c7dd45ac3dd0d650211747b032990f4d2cc6d
Michele Pittoni <michele@pittoni.org>
2013-03-14 19:33:39 +0100

    Jenkins DB settings

--- muse/settings.py
+++ muse/settings.py
@@ -164,6 +164,27 @@ LOGGING = {
     }
 }
 
+DJANGO_CONF = os.environ.get('DJANGO_CONF', 'dev')
+if DJANGO_CONF == "jenkins":
+    DATABASE_ENGINE = 'django.db.backends.postgresql_psycopg2'
+    DATABASE_NAME = 'aMuse'
+    DATABASE_PASSWORD = 'jenkins'
+    DATABASE_USER = 'jenkins'
+    DATABASE_HOST = ''
+    DATABASE_PORT = ''
+
+    DATABASES = {
+        'default': {
+            'ENGINE': DATABASE_ENGINE,  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': DATABASE_NAME,      # Or path to database file if using sqlite3.
+            'USER': DATABASE_USER,     # Not used with sqlite3.
+            'PASSWORD': DATABASE_PASSWORD,  # Not used with sqlite3.
+            'HOST': DATABASE_HOST,     # Set to empty string for localhost. Not used with sqlite3.
+            'PORT': DATABASE_PORT,     # Set to empty string for default. Not used with sqlite3.
+        }
+    }
+
+
 try:
     from settings_local import *
 except ImportError:
