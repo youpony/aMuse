bcda86adf3df4d1937911a8863e5dda43d5a420c
Luca Colleoni <luca.colle92@gmail.com>
2013-03-18 09:38:20 +0100

    improve admin interface in order to add images to items

--- muse/rest/admin.py
+++ muse/rest/admin.py
@@ -1,27 +1,56 @@
+import datetime
+import datetime
 from django.contrib import admin
-from muse.rest.models import Item, Exhibition
+from muse.rest.models import Museum, Item, Exhibition, ItemImage
+
+class MuseumAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('Museum info',
+            {'fields':['name', 'address', 'referral']
+        }),
+    )
+
+    list_filter = ['name']
+    search_fields = ['name']
+
+
+class UserImageInline(admin.StackedInline):
+    model = ItemImage
+    extra = 1
+
 
 class ItemAdmin(admin.ModelAdmin):
     fieldsets = (
-        ( 'Item info',
+        ('Item info',
             {'fields': ['name', 'desc', 'author', 'year']
         }),
-        ( 'Exhibition info',
+        ('Exhibition info',
             {'fields': ['exhibitions']
         }),
     )
 
-    list_display = ('name', 'author', 'year')
+    inlines = [UserImageInline]
+    list_display = ('name', 'author', 'year', 'actually_exposed')
     list_filter = ['author', 'year']
     search_fields = ['name']
+
+    def actually_exposed(self, obj):
+        """
+        This method chek if the item is exposed in an exhibition that is
+        actually public.
+        """
+        return obj.exhibitions.filter(end_date__gte=datetime.date.today())\
+            .count() > 0
+    actually_exposed.boolean = True
+    actually_exposed.short_description = "Actually exposed?"
 
 
 class ExhibitionAdmin(admin.ModelAdmin):
     fieldsets = (
-        ( 'Exhibition info',
+        ('Exhibition info',
             {'fields': ['museum', 'title', 'description', 'image']
         }),
-        ( 'Date information',
+        ('Date information',
             {'fields': ['start_date', 'end_date']
         }),
     )
@@ -32,5 +61,6 @@ class ExhibitionAdmin(admin.ModelAdmin):
     search_fields = ['title', 'museum']
 
 
+admin.site.register(Museum, MuseumAdmin)
 admin.site.register(Item, ItemAdmin)
 admin.site.register(Exhibition, ExhibitionAdmin)
