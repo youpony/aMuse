f353c471c7e14dec43dc566dba87ecb25322b3a9
Luca Colleoni <luca.colle92@gmail.com>
2013-04-25 10:57:26 +0200

    Adding setting for test email in settings.py

--- muse/settings.py
+++ muse/settings.py
@@ -200,6 +200,21 @@ if os.getenv('JENKINS_URL', False):
 LOGIN_REDIRECT_URL = '/administration/'
 LOGIN_URL = '/administration/login/'
 
+#configure smtp for debug
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+# Host for sending e-mail.
+EMAIL_HOST = 'localhost'
+
+# Port for sending e-mail.
+EMAIL_PORT = 1025
+
+# Optional SMTP authentication information for EMAIL_HOST.
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
+
+
 try:
     from settings_local import *
 except ImportError:
