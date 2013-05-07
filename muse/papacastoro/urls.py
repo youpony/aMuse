a41283988ed0a65e91b087df22250fd2a29d7c65
Luca Colleoni <luca.colle92@gmail.com>
2013-05-07 14:10:55 +0200

    Storyteller: start interface for storyteller edit

--- muse/papacastoro/urls.py
+++ muse/papacastoro/urls.py
@@ -1,7 +1,18 @@
 from django.conf.urls import patterns, url
 
+from muse.papacastoro import views
+
 urlpatterns = patterns(
     'muse.papacastoro.views',
+    url(
+        r'manage/(?P<private_id>\w+)/$',
+        views.PostList.as_view(),
+        name='posts_list'
+    ),
+    url(
+        r'delete/(?P<private_id>\w+)/(?P<pk>\w+)/$',
+        views.PostDelete.as_view(),
+        name='post_delete'
+    ),
     url(r'(?P<public_id>\w+)/$', 'tour', name='tour'),
 )
-
--- muse/papacastoro/views.py
+++ muse/papacastoro/views.py
@@ -1,10 +1,64 @@
+from django.views.generic.list import ListView
+from django.shortcuts import get_object_or_404
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy, reverse
+
 from django.shortcuts import render
-from muse.rest.models import Tour
+from muse.rest.models import Tour, Post
 
 
 def tour(request, public_id):
-    t = Tour.objects.get(public_id=public_id)
+    t = get_object_or_404(Tour, public_id=public_id)
     p = t.post_set.all()
     return render(request,
                   'papacastoro/index.html',
                   {'tour': t, 'posts': p, 'host': request.get_host()})
+
+
+class PostList(ListView):
+    model = Post
+    template_name = 'papacastoro/post/posts_list.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        self.tour = get_object_or_404(
+            Tour, private_id=self.kwargs['private_id']
+        )
+
+        return Post.objects.filter(tour=self.tour)
+
+    def get_context_data(self, **kwargs):
+        context = super(PostList, self).get_context_data(**kwargs)
+        context['tour_private_id'] = self.kwargs['private_id']
+        return context
+
+
+class PostDelete(DeleteView):
+    model = Post
+    template_name = 'papacastoro/post/post_confirm_delete.html'
+
+    def get_success_url(self):
+        return reverse_lazy('posts_list', args=[self.kwargs['private_id']])
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDelete, self).get_context_data(**kwargs)
+
+        context['undo_url'] = reverse(
+            'posts_list', args=[self.kwargs['private_id']]
+        )
+        return context
+
+    def delete(self, request, *args, **kwargs):
+        tour = get_object_or_404(Tour, private_id=self.kwargs['private_id'])
+        index = self.get_object().ordering_index
+        result = super(PostDelete, self).delete(request, *args, **kwargs)
+
+        posts = Post.objects.filter(tour=tour)[index:]
+
+        for post in posts:
+            post.ordering_index -= 1
+            post.save()
+
+        return result
+
+
