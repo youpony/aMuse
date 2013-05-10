4f58bad03e416f239eac4ce9f922f7f5667951c0
Luca Colleoni <luca.colle92@gmail.com>
2013-05-10 21:02:32 +0200

    Add italian translaction for rest models.py

--- muse/rest/models.py
+++ muse/rest/models.py
@@ -8,6 +8,7 @@ from django.db import models
 from django.dispatch import Signal, receiver
 from django.core.exceptions import ValidationError
 from django.core.mail import send_mail
+from django.utils.translation import ugettext_lazy as _
 
 
 def csrng_key():
@@ -23,12 +24,16 @@ class Museum(models.Model):
     A museum simply holds informations about spacial, temporal, and social
     coordinates.
     """
-    name = models.CharField(max_length=30)
-    address = models.CharField(max_length=50)
-    referral = models.EmailField(max_length=254)
+    name = models.CharField(_('name'), max_length=30)
+    address = models.CharField(_('address'), max_length=50)
+    referral = models.EmailField(_('referral'), max_length=254)
 
     def __unicode__(self):
         return self.name
+
+    class Meta:
+        verbose_name = _('museum')
+        verbose_name_plural = _('museums')
 
 
 class Exhibition(models.Model):
@@ -36,12 +41,12 @@ class Exhibition(models.Model):
     An exhibition is the setting up of a presentation of different items,
     collected differently by each user in tours.
     """
-    museum = models.ForeignKey(Museum)
-    title = models.CharField(max_length=60)
-    description = models.TextField()
-    start_date = models.DateField()
-    end_date = models.DateField()
-    image = models.ImageField(upload_to='uploads')
+    museum = models.ForeignKey(Museum, verbose_name=_('museum'))
+    title = models.CharField(_('title'), max_length=60)
+    description = models.TextField(_('description'))
+    start_date = models.DateField(_('start date'))
+    end_date = models.DateField(_('end date'))
+    image = models.ImageField(_('image'), upload_to='uploads')
     video = models.CharField(max_length=40, blank=True)
 
     def __unicode__(self):
@@ -58,19 +63,23 @@ class Exhibition(models.Model):
 
         super(Exhibition, self).save(*args, **kwargs)
 
+    class Meta:
+        verbose_name = _('exhibition')
+        verbose_name_plural = _('exhibitions')
+
 
 class Item(models.Model):
     """
     An Item is an object present at the exhibition.
     """
-    name = models.CharField(max_length=50)
-    desc = models.TextField()
-    author = models.CharField(max_length=50, blank=True)
-    year = models.CharField(max_length=9)
-    city = models.CharField(max_length=50, blank=True)
+    name = models.CharField(_('item'), max_length=50)
+    desc = models.TextField(_('description'))
+    author = models.CharField(_('author'), max_length=50, blank=True)
+    year = models.CharField(_('year'),max_length=9)
+    city = models.CharField(_('city'),max_length=50, blank=True)
     exhibitions = models.ManyToManyField(
         Exhibition,
-        verbose_name='exhibitions where this item is available'
+        verbose_name=_('exhibitions where this item is available')
     )
 
     def __unicode__(self):
@@ -89,22 +98,26 @@ class Item(models.Model):
 
         super(Item, self).save(*args, **kwargs)
 
+    class Meta:
+        verbose_name = _('item')
+        verbose_name_plural = _('items')
+
 
 class Tour(models.Model):
     """
     The user visiting the exhibition.
     """
-    public_id = models.CharField(default=csrng_key, max_length=64,
-                                 unique=True, editable=False)
-    private_id = models.CharField(default=csrng_key, max_length=64,
-                                  unique=True, editable=False)
-    name = models.CharField(max_length=60)
+    public_id = models.CharField(_('pubblic id'), default=csrng_key,
+                                 max_length=64, unique=True, editable=False)
+    private_id = models.CharField(_('private id'), default=csrng_key,
+                                  max_length=64, unique=True, editable=False)
+    name = models.CharField(_('name'), max_length=60)
     email = models.EmailField(max_length=254)
-    exhibition = models.ForeignKey(Exhibition)
+    exhibition = models.ForeignKey(Exhibition, verbose_name=_('exhibition'))
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
-        return u'{user}\'s tour'.format(user=unicode(self.name))
+        return _(u'{user}\'s tour').format(user=unicode(self.name))
 
 
 class ItemImage(models.Model):
@@ -112,12 +125,18 @@ class ItemImage(models.Model):
     This class represent image directly connect with the museum material,
     so it rapresent official images.
     """
-    title = models.CharField(max_length=80)
-    item = models.ForeignKey(Item, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='item_images/{.title:.80}'.format)
+    title = models.CharField(_('title'), max_length=80)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE,
+                             verbose_name=_('item image'), )
+    image = models.ImageField(_('image'),
+                              upload_to='item_images/{.title:.80}'.format)
 
     def __unicode__(self):
         return self.title
+
+    class Meta:
+        verbose_name = _('item image')
+        verbose_name_plural = _('item images')
 
 
 class Post(models.Model):
@@ -125,15 +144,17 @@ class Post(models.Model):
     A post is some kind of content present/taken at the exhibition,
     that the user wants to share.
     """
-    ordering_index = models.IntegerField()
+    ordering_index = models.IntegerField(_('ordering index'))
     tour = models.ForeignKey(Tour)
     item = models.ForeignKey(
-        Item, on_delete=models.PROTECT, blank=True, null=True
+        Item, on_delete=models.PROTECT, blank=True, null=True,
+        verbose_name=_('item')
     )
     image = models.ImageField(
+        _('image'),
         upload_to='people_uploads', blank=True, null=True
     )
-    text = models.TextField(blank=True, null=True)
+    text = models.TextField(_('text'), blank=True, null=True)
     # XXX. add pointer to the current exhibition.
 
     def save(self, *args, **kwargs):
@@ -154,6 +175,9 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['ordering_index']
+        verbose_name = _('post')
+        verbose_name_plural = _('posts')
+
 
 
 #story_created = Signal(providing_args=['tour', 'museum'])
--- muse/settings.py
+++ muse/settings.py
@@ -111,7 +111,7 @@ MIDDLEWARE_CLASSES = (
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'breadcrumbs.middleware.BreadcrumbsMiddleware',
-
+    'django.middleware.locale.LocaleMiddleware',
 )
 
 ROOT_URLCONF = 'muse.urls'
@@ -137,12 +137,19 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.request"
 )
 
+_ = lambda s: s
+
+LANGUAGES = (
+  ('en', _('English')),
+  ('it', _('Italian')),
+)
+
 INSTALLED_APPS = (
     'muse.kiosk',
     'muse.rest',
     'muse.papacastoro',
     'muse.administration',
-    
+
     'crispy_forms',
     'breadcrumbs',
     'south',
